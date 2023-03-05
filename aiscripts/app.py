f30b9453cf430f413e2c1e343c58da360329ee60
energycomp56 <energycomp56@gmail.com>
2023-03-05 15:57:42 -0800

    Added openAI API

--- aiscripts/app.py
+++ aiscripts/app.py
@@ -4,7 +4,7 @@ from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 import openai
 from pydantic import BaseModel
-openai.api_key = "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"
+openai.api_key = "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"
 
 app = FastAPI()
 app.add_middleware(
@@ -15,20 +15,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/ApBioCurriculum")
-async def home():
-    APBioCurrculum = "prompt"
+@app.get("/Ap-Bio-Curriculum")
+def home():
     try:
-        answer = openai.Completion.create(engine="text-ada-001", prompt = APBioCurrculum, max_tokens=10).choices[0].text
-    except:
-        answer = "I am not sure what to say"
+        answer = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a system that generates questions based only on the following curriculum: ${ApBioCurriculum}"},
+                {"role": "user", "content": "Provide 5 multiple choice questions for AP biology in a JSON format: { questions: [ question: <question>, choices: [<choice>, <choice>], correct_choice: <correct choice>, answer: <answer in the form of a sentence> ], ... }"},
+            ])
+        print(answer)
+    except Exception as e:
+        print(e)
+        answer = e
     return answer
 
 @app.post("/")
 def test(prompt: str):
     question = prompt
     try:
-        answer = openai.Completion.create(engine="text-ada-001", prompt = question, max_tokens=10).choices[0].text
+        answer = openai.ChatCompletion.create(engine="gpt-3.5-turbo", prompt = question, max_tokens=10).choices[0].text
     except:
         answer = "I am not sure what to say"
     return answer
+
+ApBioCurriculum = """
+    Unit 1: Chemistry of Life 8–11% 
+    Unit 2: Cell Structure and Function 10–13% 
+    Unit 3: Cellular Energetics 12–16% 
+    Unit 4: Cell Communication and Cell Cycle 10–15% 
+    Unit 5: Heredity 8–11% 
+    Unit 6: Gene Expression and Regulation 12–16% 
+    Unit 7: Natural Selection 13–20% 
+    Unit 8: Ecology 10–15%"""
